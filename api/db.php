0acf5ddaafc21dfe9802b3a46734073fadaf290f
kiyumb2 <wendkiyu@gmail.com>
2026-08-21 13:59:06 +0300

    db.php

--- api/db.php
+++ api/db.php
@@ -1,14 +1,13 @@
 <?php
 // Central DB connection for Supabase Pooler (IPv4 Compatible)
-$host = 'eu-west-1.pooler.supabase.com';
+$host = 'aws-0-eu-central-1.pooler.supabase.com';
 $port = '6543';
 $db   = 'postgres';
 $user = 'postgres.vxpqsbjjegvkjusiktxd';
 $pass = '0926440279@Kiyu';
 
 try {
-    // We add the project option parameter to ensure Supavisor routes to your tenant correctly
-    $dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require;options='--project=vxpqsbjjegvkjusiktxd'";
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
     
     $pdo = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
